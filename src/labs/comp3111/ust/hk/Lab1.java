652fa32aa22135e63ad03277c58cd39ee2702f6a
Xelios Chang <Xelios Chang@10.89.3.234>
2018-02-12 23:47:22 +0800

    Added a congratulation message

--- src/labs/comp3111/ust/hk/Lab1.java
+++ src/labs/comp3111/ust/hk/Lab1.java
@@ -5,5 +5,6 @@ public class Lab1 {
 	public static void main(String[] args)
 	{
 		System.out.println("Lab1: Introduction to Git and GitHub");
+		System.out.println("Congratulation! You learned the Git Basics");
 	}
 }
